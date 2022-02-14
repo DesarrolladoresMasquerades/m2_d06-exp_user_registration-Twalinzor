3264168bbbe5ace0f400114b41ea312a5038de18
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-02-14 12:22:46 +0100

    .

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const User = require('../models/User.model');
 const router = express.Router();
-const saltRound = 10;
+const saltRounds = 5;
 const bcrypt = require('bcrypt');
 
 router.route('signup')
 	.get((req, res)=>{
 		res.render('signup');
 	})
-	.post((req, res)=>{})
+	.post((req, res)=>{
+		const username = req.body.username;
+		const password = req.body.password;
+
+		//Check the form is NOT EMPTY
+		if(!username || !password) 
+			res.render("signup", {errorMessage: "All fields are required, u moron."});
+
+		User.find({username})
+			.then(user=>{
+				if(user){res.render("signup", {errorMessage: "User already taken"})}
+
+				const salt = bcrypt.genSaltSync(saltRound)
+				const hashedPwd = bcrypt.hashSync(password, salt)
+
+				User.create({username, password: hashedPwd})
+			})
+	})
 
 router.get('/login', (req, res) => {
 	res.render('login');
